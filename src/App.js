992843c1ddc928fbda4bd639c3cd5b3cb520cf46
keithpops <keith.poplawski@gmail.com>
2018-05-19 16:21:34 -0400

    adds vudu / removes boilerplate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import v from 'vudu'
+import logo from 'logo.svg'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const localClasses = v({
+  app: {
+    textAlign: 'center',
+  },
+  header: {
+    backgroundColor: '#222',
+    height: '150px',
+    padding: '20px',
+    color: 'white',
+  },
+  logo: {
+    animation: 'spin infinite 20s linear',
+    display: 'inline-block',
+    height: '80px',
+    '@keyframes spin': {
+      from: {
+        transform: 'rotate(0deg)',
+      },
+      to: {
+        transform: 'rotate(360deg)',
+      },
+    },
+  },
+  title: {
+    fontSize: '1.5em',
+  },
+  intro: {
+    fontSize: 'large',
+  },
+})
 
-export default App;
+const App = () => (
+  <div className={localClasses.app}>
+    <header className={localClasses.header}>
+      <img src={logo} className={localClasses.logo} alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className={localClasses.intro}>
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+)
+
+export default App
